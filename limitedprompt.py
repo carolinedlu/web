3ed16e9a9a02568eb12024dc7cadea3e5d8336ee
scooter7 <scooter.vineburgh@gmail.com>
2023-05-03 23:10:43 -0500

    Update limitedprompt.py

--- limitedprompt.py
+++ limitedprompt.py
@@ -1,4 +1,3 @@
-import logging
 import streamlit as st
 from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
@@ -64,52 +63,6 @@ def chatbot(input_text, first_name, email):
     os.makedirs(content_dir, exist_ok=True)
 
     # Write the user question and chatbot response to a file in the content directory
-    filename = st.session_state.filename
-    file_path = os.path.join(content_dir, filename)
-    with open(file_path, 'a') as f:
-        f.write(f"{first_name} ({email}): {input_text}\n")
-        f.write(f"Chatbot response: {response.response}\n")
-        
-    # Write the chat file to GitHub
-    with open(file_path, 'rb') as f:
-        contents = f.read()
-        repo.create_file(f"content/{filename}", f"Add chat file {filename}", contents)
-
-    return response.response
-
-index = construct_index()
-
-st.set_page_config(page_title="Carnegie Chatbot")
-
-# Initialize first_prompt_sent to False in session state
-if "first_prompt_sent" not in st.session_state:
-    st.session_state.first_prompt_sent = False
-
-# Create a container to hold the chat messages
-chat_container = st.container()
-
-# Create a form to enter a message and submit it
-form = st.form(key="my_form", clear_on_submit=True)
-
-if not st.session_state.first_prompt_sent:
-    get_first_prompt()
-    st.session_state.first_prompt_sent = True
-else:
-    if "submit_clicked" in st.experimental_get_query_params():
-        first_name = form.text_input("Enter your first name:", key="first_name")
-        email = form.text_input("Enter your email address:", key="email")
-        
-        input_text = form.text_input("Enter your message:")
-        form_submit_button = form.form_submit_button(label="Send")
-
-        if form_submit_button and input_text:
-            # Set the filename key every time the form is submitted
-            filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.docx")
-            st.session_state.filename = filename
-
-            response = chatbot(input_text, first_name, email)
-
-        # Write the user message and chatbot response to the chat container
         with chat_container:
             st.write(f"{first_name}: {input_text}")
             st.write(f"Chatbot: {response}")
