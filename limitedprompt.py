cb5f2215ae7f34a55be630c493655753a32c8374
scooter7 <scooter.vineburgh@gmail.com>
2023-05-03 23:54:28 -0500

    Update limitedprompt.py

--- limitedprompt.py
+++ limitedprompt.py
@@ -39,21 +39,8 @@ def get_first_prompt():
     program_type = st.selectbox("Select program type:", ["Undergraduate", "Graduate", "Certificate", "Other"])
     region = st.selectbox("Select region:", ["Northeast", "Midwest", "South", "West"])
 
-    # Add submit button for dropdowns
-    if st.button("Select all dropdowns"):
-        # Save the selected values in session state
-        st.session_state.institution_type = institution_type
-        st.session_state.degree_level = degree_level
-        st.session_state.program_type = program_type
-        st.session_state.region = region
-
-        # Set the flag to True
-        st.session_state.dropdowns_selected = True
-
-    # Check if all dropdowns are selected before moving to the name, email, and text input view
-    if "dropdowns_selected" in st.session_state and st.session_state.dropdowns_selected:
-        # Add a delay to give time for the page to update
-        st.experimental_set_query_params(submit_clicked=True)
+    first_prompt = f"I am interested in learning about the following types of colleges: {institution_type}, {degree_level}, {program_type}, and {region} region of the US."
+    return first_prompt
 
 def chatbot(input_text, first_name, email):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
@@ -61,15 +48,70 @@ def chatbot(input_text, first_name, email):
 
     # Create the content directory if it doesn't already exist
     content_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "content")
+
     os.makedirs(content_dir, exist_ok=True)
 
     # Write the user question and chatbot response to a file in the content directory
+    filename = st.session_state.filename
+    file_path = os.path.join(content_dir, filename)
+    with open(file_path, 'a') as f:
+        f.write(f"{first_name} ({email}): {input_text}\n")
+        f.write(f"Chatbot response: {response.response}\n")
+        
+    # Write the chat file to GitHub
+    with open(file_path, 'rb') as f:
+        contents = f.read()
+        repo.create_file(f"content/{filename}", f"Add chat file {filename}", contents)
 
+    return response.response
+
+index = construct_index()
+
+st.set_page_config(page_title="Carnegie Chatbot")
+
+# Initialize first_prompt_sent to False in session state
+if "first_prompt_sent" not in st.session_state:
+    st.session_state.first_prompt_sent = False
+
+# Create a container to hold the chat messages
+chat_container = st.container()
+
+# Initialize last_send_pressed to False in session state
+if "last_send_pressed" not in st.session_state:
+    st.session_state.last_send_pressed = False
+
+# Create a form to enter a message and submit it
+form = st.form(key="my_form", clear_on_submit=True)
+if "first_send" not in st.session_state:
+    st.session_state.first_send = True
+
+if st.session_state.first_send:
+    first_name = form.text_input("Enter your first name:", key="first_name")
+    email = form.text_input("Enter your email address:", key="email")
+    st.session_state.first_send = False
+else:
+    first_name = st.session_state.first_name
+    email = st.session_state.email
+
+input_text = form.text_input("Enter your message:")
+form_submit_button = form.form_submit_button(label="Send")
+
+if form_submit_button and input_text:
+    # Set the filename key every time the form is submitted
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.docx")
+    st.session_state.filename = filename
+    
+    response = chatbot(input_text, first_name, email)
+
+    # Write the user message and chatbot response to the chat container
     with chat_container:
         st.write(f"{first_name}: {input_text}")
         st.write(f"Chatbot: {response}")
-        st.session_state.first_name = first_name
-        st.session_state.email = email
 
-    # Clear the input field after sending a message
-    form.empty()
+    # Save the first name and email in session state
+    st.session_state.first_name = first_name
+    st.session_state.email = email
+
+# Clear the input field after sending a message
+form.empty()
+
