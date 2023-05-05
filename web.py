56d781c9609ca92f425442af702894326a1f84e1
scooter7 <scooter.vineburgh@gmail.com>
2023-05-05 00:13:59 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,65 +1,28 @@
-import logging
-import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-from gpt_index import GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain.chat_models import ChatOpenAI
-import sys
-from datetime import datetime
-import os
-from github import Github
+from llama_index import GPTSimpleVectorIndex, download_loader
+from langchain.agents import initialize_agent, Tool
+from langchain.llms import OpenAI
+from langchain.chains.conversation.memory import ConversationBufferMemory
 
-if "OPENAI_API_KEY" not in st.secrets:
-    st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
-    sys.exit(1)
+BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+loader = BeautifulSoupWebReader()
+documents = loader.load_data(urls=['https://google.com'])
+index = GPTSimpleVectorIndex(documents)
 
-logging.info(f"OPENAI_API_KEY: {openai_api_key}")
+tools = [
+    Tool(
+        name="Website Index",
+        func=lambda q: index.query(q),
+        description=f"Useful when you want answer questions about the text on websites.",
+    ),
+]
+llm = OpenAI(temperature=0)
+memory = ConversationBufferMemory(memory_key="chat_history")
+agent_chain = initialize_agent(
+    tools, llm, agent="zero-shot-react-description", memory=memory
+)
 
-# Set up the GitHub API
-g = Github(st.secrets["GITHUB_TOKEN"])
-repo = g.get_repo("scooter7/web")
-
-def construct_index(directory_path):
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
-
-    documents = SimpleDirectoryReader(directory_path).load_data()
-
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    index.directory_path = directory_path
-
-    index.save_to_disk('index.json')
-
-    return index
-
-# Scrape text from URLs
-urls = ["https://en.wikipedia.org/wiki/Python_(programming_language)", 
-        "https://www.python.org/", 
-        "https://docs.python.org/3/tutorial/index.html",
-        "https://www.tutorialspoint.com/python/index.htm",
-        "https://realpython.com/",
-        "https://www.geeksforgeeks.org/python-programming-language/",
-        "https://www.codecademy.com/learn/learn-python",
-        "https://www.w3schools.com/python/",
-        "https://www.datacamp.com/courses/intro-to-python-for-data-science",
-        "https://www.learnpython.org/"]
-
-for i, url in enumerate(urls):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    text = soup.get_text()
-    filename = f"docs/{i}.txt"
-    with open(filename, "w") as f:
-        f.write(text)
+output = agent_chain.run(input="What language is on this website?")
 
 def chatbot(input_text, first_name, email):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
