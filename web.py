328db09c0249cf4d526b0f7cfb636bde6f8711f0
scooter7 <scooter.vineburgh@gmail.com>
2023-05-04 23:39:13 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,20 +1,15 @@
 import logging
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-from gpt_index import GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain.chat_models import ChatOpenAI
+import sys
 from datetime import datetime
 import os
 from github import Github
 
 if "OPENAI_API_KEY" not in st.secrets:
     st.error("Please set the OPENAI_API_KEY secret on the Streamlit dashboard.")
-    st.stop()
-
-if "GITHUB_TOKEN" not in st.secrets:
-    st.error("Please set the GITHUB_TOKEN secret on the Streamlit dashboard.")
-    st.stop()
+    sys.exit(1)
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
 
@@ -22,11 +17,27 @@ logging.info(f"OPENAI_API_KEY: {openai_api_key}")
 
 # Set up the GitHub API
 g = Github(st.secrets["GITHUB_TOKEN"])
-repo = g.get_repo("scooter7/web")
+repo = g.get_repo("scooter7/CXBot")
 
-# Create docs directory
-if not os.path.exists("docs"):
-    os.makedirs("docs")
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.directory_path = directory_path
+
+    index.save_to_disk('index.json')
+
+    return index
 
 # Scrape text from URLs
 urls = ["https://en.wikipedia.org/wiki/Python_(programming_language)", 
